1f3e47bdbe6ab589d7f937b95bd408a0374a9cec
Voltaicforge <19149206+Truxnell@users.noreply.github.com>
2021-12-11 23:29:22 +1100

    [2] Completed day 2

--- 2021/py/day02.py
+++ 2021/py/day02.py
@@ -1,5 +1,3 @@
-
-
 def day02a(lines):
 
     h = 0
@@ -15,7 +13,7 @@ def day02a(lines):
         elif a[0] == "up":
             d -= a[1]
 
-    return h*d
+    return h * d
 
 
 def day02b(lines):
@@ -29,7 +27,7 @@ def day02b(lines):
         w[1] = int(w[1])
         if w[0] == "forward":
             h += w[1]
-            d += w[1]*a
+            d += w[1] * a
         elif w[0] == "down":
             a += w[1]
         elif w[0] == "up":
@@ -37,15 +35,23 @@ def day02b(lines):
         else:
             raise Exception("How did you get here?")
 
-    return h*d
+    return h * d
 
 
-ex1 = ['forward 5', 'down 5', 'forward 8', 'up 3', 'down 8', 'forward 2']
+ex1 = ["forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"]
 
 
-def test_02_ex1(): assert day02a(ex1) == 150
-def test_02_ex2(): assert day02b(ex1) == 900
+def test_02_ex1():
+    assert day02a(ex1) == 150
 
 
-def test_02a(day02_lines): assert day02a(day02_lines) == 2147104
-def test_02b(day02_lines): assert day02b(day02_lines) == 2044620088
+def test_02_ex2():
+    assert day02b(ex1) == 900
+
+
+def test_02a(day02_lines):
+    assert day02a(day02_lines) == 2147104
+
+
+def test_02b(day02_lines):
+    assert day02b(day02_lines) == 2044620088
