a86f32e10d50f2cfc48be969a91910b8bf185eb1
Nat Allan <19149206+Truxnell@users.noreply.github.com>
2022-12-07 19:58:10 +1100

    [day07] wip

--- 2022/py/day07.py
+++ 2022/py/day07.py
@@ -1,8 +1,52 @@
 from aocd import get_data
 
-EXAMPLE = []
+EXAMPLE = [
+    "$ cd /",
+    "$ ls",
+    "dir a",
+    "14848514 b.txt",
+    "8504156 c.dat",
+    "dir d",
+    "$ cd a",
+    "$ ls",
+    "dir e",
+    "29116 f",
+    "2557 g",
+    "62596 h.lst",
+    "$ cd e",
+    "$ ls",
+    "584 i",
+    "$ cd ..",
+    "$ cd ..",
+    "$ cd d",
+    "$ ls",
+    "4060174 j",
+    "8033020 d.log",
+    "5626152 d.ext",
+    "7214296 k",
+]
+
 YEAR = 2022
 DAY = 7
+
+
+class Storage:
+    def __init__(self):
+        self.cwd = "/"
+        self.dir = {"/": []}
+
+    def cd(self, path: str):
+        if path == "/":
+            self.cwd = "/"
+            return
+        
+        if path=="..":
+            self.cwd=self.cwd[:self.cwd.rfind("/")] 
+            return
+
+        self.cwd+="/"
+        self.cwd+=path
+
 
 
 def solve_puzzle(puzzle: str, part_a=True) -> int:
@@ -17,3 +61,5 @@ def test_examples_pt_a():
 def test_pt_a():
     inp = get_data(day=DAY, year=YEAR)
     assert solve_puzzle(inp) == 0
+
+"".rindex)
